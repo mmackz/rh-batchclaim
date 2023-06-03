2b062acfc6128ee355da6abc7e3df593ba269345
Mmackz <matthewmackenz@gmail.com>
2023-06-03 14:49:03 -0700

    ⬆️ use built-in next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import "@/styles/reset.css";
 import "@/styles/globals.css";
 import { Providers } from "./providers";
-import { DM_Sans } from "@next/font/google"
-import localFont from "@next/font/local";
+import { DM_Sans } from "next/font/google";
+import localFont from "next/font/local";
 
 const font = localFont({
    src: [
       { path: "../fonts/CircularStd-Book.otf", weight: "400" },
-      { path:  "../fonts/circular-std-medium-500.ttf", weight: "500" },
+      { path: "../fonts/circular-std-medium-500.ttf", weight: "500" },
       { path: "../fonts/CircularStd-Bold.ttf", weight: "700" }
    ]
 });
 
-const dmSans = DM_Sans({ weight: "500", subsets: ["latin"], variable: "--address-font"});
+const dmSans = DM_Sans({ weight: "500", subsets: ["latin"], variable: "--address-font" });
 
 export const metadata = {
    title: "Rabbithole Rewards Batch Claim",
