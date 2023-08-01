d553669c2e9dd7461476e810b0c9324dfcc895a7
Mmackz <matthewmackenz@gmail.com>
2023-07-31 20:06:53 -0700

    ✨ show warning when no wallet connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,27 @@ import RewardList from "@/components/RewardList";
 import { useAccount } from "@/modules/wagmi/dist/index";
 
 function Page() {
-
    const { address, isConnected } = useAccount();
 
    return (
       <>
-         <Header/>
-         { isConnected && <RewardList address={address} /> }
-         
-         
+         <Header />
+         {isConnected ? (
+            <RewardList address={address} />
+         ) : (
+            <div
+               style={{
+                  width: "fit-content",
+                  padding: "1rem",
+                  background: "white",
+                  borderRadius: "0.5rem",
+                  border: "3px solid black",
+                  margin: "2rem auto"
+               }}
+            >
+               Please Connect Your Wallet
+            </div>
+         )}
       </>
    );
 }
